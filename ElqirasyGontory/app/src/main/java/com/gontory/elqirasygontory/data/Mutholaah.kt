da567b775e6370bd988e47416c0c9040628dd7d7
aafif <aafifpratama@gmail.com>
2024-01-06 08:27:00 +0700

    [add] added ElQirasyGontory Project

---
+++ ElqirasyGontory/app/src/main/java/com/gontory/elqirasygontory/data/Mutholaah.kt
@@ -0,0 +1,18 @@
+package com.gontory.elqirasygontory.data
+
+import android.os.Parcelable
+import com.google.firebase.firestore.DocumentId
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Mutholaah(
+    @DocumentId
+    val mutholaahId: String,
+    val urutan: Int,
+    val img_url: String,
+    val title: String,
+    val video_url: String,
+    val questions: Long
+) : Parcelable {
+    constructor(): this("",0, "", "", "",0)
+}
